4cf8045ce904afaf4d4d3930d1c3d20af0df24b9
Umar farooq <ufservices465@gmail.com>
2023-10-27 20:44:26 +0530

    Local_global_scope

---
+++ Local_Global.js
@@ -0,0 +1,27 @@
+// // Block scope
+
+// {
+//     let a = 8;
+
+// }
+// // console.log(a); this will show a is undefined
+
+// // let and const are block scope
+
+// // var is global scope
+
+// {
+//     var b = 8;
+
+// }
+
+// console.log(b); // var value will print
+
+// function scope
+
+let p = 10;
+function ax() {
+    console.log(p);
+}
+
+ax()
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,18 @@
+// Block scope
+
+// {
+//     let a = 8;
+
+// }
+// // console.log(a); this will show a is undefined
+
+// // let and const are block scope
+
+// // var is global scope
+
+// {
+//     var b = 8;
+
+// }
+
+// console.log(b);
